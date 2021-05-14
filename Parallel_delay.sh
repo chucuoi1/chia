1eef756dac98039c89be17fa17a900f06e20ebbb
chucuoi1 <khang140388@yahoo.com>
2021-05-14 09:18:29 -0600

    Create Parallel_delay.sh

---
+++ Parallel_delay.sh
@@ -0,0 +1,5 @@
+chia plots create -k 32 -b 4000 -n 10 -t "ổ Vẽ" -d "ổ Chứa"&
+sleep 3600
+chia plots create -k 32 -b 4000 -n 10 -t "ổ Vẽ" -d "ổ Chứa"&
+sleep 3600
+chia plots create -k 32 -b 4000 -n 10 -t "ổ Vẽ" -d "ổ Chứa"&
